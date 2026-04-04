59f0b53dc7cfc24699dcd6d65455144d1236e5a2
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2026-04-04 15:32:13 -0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+//add inter font for it to be applied thoughout the application
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // the Tailwind antialiased class smooths out the font
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// this file is used to keep the fonts that will be used throughout the application
+
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
