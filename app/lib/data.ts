2d8df6f7cca116397d9abc22508a114ceed4bb1e
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2026-04-04 18:54:43 -0600

    added more elements to dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,19 +9,12 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.nextjs_POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -30,6 +23,7 @@ export async function fetchRevenue() {
   }
 }
 
+//fetch the last 5 invoices
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw[]>`
