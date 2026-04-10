06c0636207c3f70406ceca8c821de22dea35d863
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2026-04-10 11:51:13 -0600

    added partial metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 // this is root layout. Any UI added here will be shared across all pages of the app. 
 
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 //add inter font for it to be applied thoughout the application
 import { inter } from '@/app/ui/fonts';
 
